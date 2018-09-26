c1aa1088a5110e8d0ac90970807e0d8a95e4a009
IagoLast <iago.lastra@gmail.com>
2018-09-26 12:22:51 +0200

    First client example

--- packages/consumer/index.js
+++ packages/consumer/index.js
@@ -1,12 +1,37 @@
-// Create WebSocket connection.
-const socket = new WebSocket('ws://10.0.32.102:3333');
+class RT3Consumer {
+  constructor(url, {
+    onSet,
+    onDelete
+  }) {
+    this.onSet = onSet;
+    this.onDelete = onDelete;
 
-// Connection opened
-socket.addEventListener('open', function (event) {
-  socket.send('Hello Server!');
-});
+    this._socket = new WebSocket(url);
 
-// Listen for messages
-socket.addEventListener('message', function (event) {
-  console.log('Message from server ', event.data);
-});
+    this._socket.addEventListener('open', event => {
+      this._socket.send('Hello Server!');
+    });
+    this._socket.addEventListener('message', event => {
+      this.onEvent(event);
+    });
+  }
+
+  onEvent(event) {
+    const message = JSON.parse(event.data);
+    switch (message.type) {
+      case 'set':
+        {
+          const { lat, lon, id, data} = message;
+          this.onSet && this.onSet({lat, lon, id, data });
+          break;
+        }
+      case 'delete':
+        {
+          const {id} = message;
+          this.onDelete && this.onDelete({id});
+          break;
+        }
+    }
+  }
+
+}
