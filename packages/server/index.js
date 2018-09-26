5a319e8b34ecbe6068a02783b9af4babc65d6a75
Simon Martín <oleurud@gmail.com>
2018-09-26 14:52:10 +0200

    broadcast

--- packages/server/index.js
+++ packages/server/index.js
@@ -5,36 +5,77 @@ const fixtures = require('./config/fixtures.json')
 const server = new http.createServer();
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', function connection(ws, req) {
-    const ip = req.connection.remoteAddress;
 
-    ws.on('message', function incoming(message) {
-        console.log(`client[${ip}]: ${message}`);
+wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+            send(client, data);
+        }
+    });
+};
+
+wss.on('connection', function connection(ws, req) {
+    ws.on('message', function incoming(point) {
+        const ip = req.connection.remoteAddress;
+        console.log(`client[${ip}]: ${point}`);
+
+        // validate point
+        if (!validateNewPoint(point)) {
+            return;
+        }
+
+        // Broadcast to everyone else
+        wss.broadcast(point);
     });
 
-    let point = fixtures[0];
-    let i = 0;
-    setInterval(function() {
-        point.lat = Math.random() * 90
-        point.lon = Math.random() * 180
-        point.data.foo = i++ % 50
-        ws.send(JSON.stringify(prepareSet(point)));
-    }, 50)
-
-    // fixtures.forEach(point => {
-
-    //     ws.send(JSON.stringify(prepareSet(point)));
-    // });
-
-    // ws.send(JSON.stringify(prepareDelete(fixtures[3])));
+    // send current state
+    sendFixtures(ws);
 });
 
+function send(ws, data) {
+    if (ws.readyState === WebSocket.OPEN) {
+        ws.send(JSON.stringify(prepareSet(data)));
+    } else {
+        error(new Error('WebSocket is not open'), ws);
+    }
+}
+
+function error(error, ws) {
+    console.error(error);
+
+    if (ws) {
+        ws.terminate();
+    }
+}
+
+function validateNewPoint(event) {
+    return typeof event == 'object' && event.type && typeof event.type == 'string'
+}
+
+function sendFixtures(ws) {
+    let point = fixtures[0];
+
+    let i = 0;
+    const interval = setInterval(function () {
+        point.lat = Math.random() * 180 - 90
+        point.lon = Math.random() * 360 - 180
+        point.data.foo = i++ % 50
+
+        if (ws.readyState === WebSocket.OPEN || i < 100) {
+            send(ws, prepareSet(point));
+        } else {
+            clearInterval(interval)
+            ws.terminate();
+        }
+    }, 100);
+}
+
 function prepareSet(point) {
-    return Object.assign({type: 'set'}, point);
+    return Object.assign({ type: 'set' }, point);
 }
 
 function prepareDelete(point) {
-    return Object.assign({type: 'delete'}, {id: point.id});
+    return Object.assign({ type: 'delete' }, { id: point.id });
 }
 
 server.listen(3333);
