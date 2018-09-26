f2f51904b92c80526b0c95ecfb63e60e5f958a01
IagoLast <iago.lastra@gmail.com>
2018-09-26 15:25:06 +0200

    Better consumer UI

--- packages/consumer/example/app.js
+++ packages/consumer/example/app.js
@@ -1,7 +1,14 @@
 import RT3Consumer from '../index.js';
 
 const callbacks = {
-  onSet: data => console.log('onSet:', data),
+  onSet,
   onDelete: data => console.log('onDelete', data)
 };
 const client = new RT3Consumer('ws://10.0.32.102:3333', callbacks);
+
+
+function onSet(data) {
+  const p = document.createElement('p');
+  p.innerHTML = `id:[${data.id}] lat: [${data.lat}] lon:[${data.lon}] data: [${JSON.stringify(data.data)}]`;
+  document.body.appendChild(p);
+}
