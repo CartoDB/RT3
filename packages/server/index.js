0f9937e5de19bf41ba22e59450be933bd40e3ab0
Simon Martín <oleurud@gmail.com>
2018-09-26 12:49:22 +0200

    faster

--- packages/server/index.js
+++ packages/server/index.js
@@ -5,16 +5,28 @@ const fixtures = require('./config/fixtures.json')
 const server = new http.createServer();
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, req) {
+    const ip = req.connection.remoteAddress;
+
     ws.on('message', function incoming(message) {
-        console.log('received: %s', message);
+        console.log(`client[${ip}]: ${message}`);
     });
 
-    fixtures.forEach(point => {
+    let point = fixtures[0];
+    let i = 0;
+    setInterval(function() {
+        point.lat = Math.random() * 90
+        point.lon = Math.random() * 180
+        point.data.foo = i++ % 50
         ws.send(JSON.stringify(prepareSet(point)));
-    });
+    }, 50)
 
-    ws.send(JSON.stringify(prepareDelete(fixtures[3])));
+    // fixtures.forEach(point => {
+
+    //     ws.send(JSON.stringify(prepareSet(point)));
+    // });
+
+    // ws.send(JSON.stringify(prepareDelete(fixtures[3])));
 });
 
 function prepareSet(point) {
